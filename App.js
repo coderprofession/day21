88ebd872eff57b4b35fcae090e15e3e432ff7660
coderprofession <coderprofession@gmail.com>
2022-08-09 23:25:18 -0700

    newly added app, index files

---
+++ App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import Listbyevent from "./component-test/Listbyevent";
+function App() {
+  return <div className="app">Day 20-21 React Testing using Jest
+  <Listbyevent/>
+  </div>;
+}
+
+export default App;
